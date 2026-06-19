e9e31a050fa965ed6383f02e903053ec6b8dcb0d
Dziadu9943 <dziadugaming89@gmail.com>
2026-06-19 21:22:22 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -85,6 +85,7 @@ function Index() {
   const lanyard = useLanyard(CONFIG.discordId);
   const user = lanyard?.discord_user;
   const status: string = lanyard?.discord_status ?? "offline";
+console.log("DEBUG: Status z Lanyard to:", status); // statsu check
   const avatarUrl = "https://cdn.discordapp.com/avatars/646259904960528385/4cdebcdcf8cfd08faaaabca5ebc6e120.png?size=128";
   
   useEffect(() => {
